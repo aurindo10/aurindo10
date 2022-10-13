3d724b8d16ca5c89017264b8eb0a51b186e17595
Aurindo Neto <105182999+aurindo10@users.noreply.github.com>
2022-10-13 11:13:17 -0300

    atualizando comparador

--- Controladores/comparador.js
+++ Controladores/comparador.js
@@ -1,90 +1,33 @@
-
 const PriceList = require('../Models/priceList')
 const Listcomparada = require('../Models/listcotada')
-
 
 
 const comparador = {
     compara:  async (req, res)=>{
         const CotacaoId = req.params.id
         const cotacoes = await PriceList.find({cotacao_id:CotacaoId})
-        const products = cotacoes.map((e)=>{return e.listOfProducts.map((i)=>{return i.valorUnitario})}) 
-        console.log(products)
-        const item = [];
-         for (let e = 0; e<products[e]?.length|| 0; e++){
-
-            
-            for (let i = 0; i<products.length; i++){
-                item.push(products[i][e])
-                console.log(products[i])
-            }
-            
-            }
-            console.log(item)
-            const novoArray = []
-            for (var i = 0; i < item.length; i = i + products.length) {
-                novoArray.push(item.slice(i, i + products.length));
-                
-              }
-            const productsName = cotacoes[0].listOfProducts.map((i)=>{return i.productName})
+        const allProductMergedinArrayInsideArray = cotacoes.map((e)=>{
+            return (e.listOfProducts.map((i)=>{return i}))
+        })
+        const allProductMerged = allProductMergedinArrayInsideArray.flat(1)
+        const idOfEachProdut = cotacoes[0].listOfProducts.map((e)=>{return e.product_id})
+        const productsInArrayofArrayById = idOfEachProdut.map((i)=>{return (allProductMerged.filter((e)=>{return e.product_id == i}))})
+        res.send(cotacoes)
 
 
-            const mergedPriceWithName =  []
-
-            for (let i = 0; i<productsName.length; i++){
-                mergedPriceWithName.push({nome: productsName[i], prices: novoArray[i] })
-   
-            }
-
-            const lowerValue = mergedPriceWithName.map((e)=>{return e.prices.findIndex((i)=>{return i===Math.min(...e.prices)})})
-
-
-        
-
-        // const listReady = cotacoes.map((e)=>{return e.findIndex((i)=>{return i===e})})
-        const items = []
-        for  (let i = 0; i<lowerValue.length; i++){
-            for  (let e = 0; e<cotacoes.length; e++){
-                if (lowerValue[i]==e) {
-                    items.push( cotacoes[e].vendedor)
-                }
-            }
-        }
-        const listall = []
-        for ( i = 0; i<cotacoes.length; i++){
-            
-           for (e = 0; e<items.length; e++){
-            if (cotacoes[i].vendedor===items[e]){
-                const hs = {seller: cotacoes[i].vendedor,produtos:cotacoes[i].listOfProducts[e]}
-            listall.push(hs)
-            }}
-
-        }
-
-
-
-            const sellersName = cotacoes.map((e)=>{return {
-                vendedor: e.vendedor,
-                 cotacao_id:e.cotacao_id, 
-                 listOfProducts: listall.filter((u)=>{
-                return u.seller === e.vendedor
-
-            })}})
-
-
-
-         for (i=0;i<sellersName.length; i++){
-                if (sellersName[i].listOfProducts[0]){
-                    const newBuyList  =  new Listcomparada (sellersName[i])
-                    newBuyList.save()
-                    res.send(newBuyList)
-                }
-        }
-        
+        const minPrices = productsInArrayofArrayById.map ( (e)=>{
+            const checker = (o, i) => typeof(o) === 'object'
+            return (e.length && e.reduce(function(prev, curr){
+                const prevOk = checker(prev, curr);
+                const currOk = checker(curr, curr);
+                if (!prevOk && !currOk) return {};
+                if (!prevOk) return curr;
+                if (!currOk) return prev;
+                return prev.valorUnitario < curr.valorUnitario ? prev : curr; 
+            })) || null;
+         })
+         console.log(minPrices)
     }
-
-
 }
-
 
 module.exports = comparador
