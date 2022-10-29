991be0d92b47e535c094c27c485465d8457fb91b
Aurindo Neto <105182999+aurindo10@users.noreply.github.com>
2022-10-29 20:57:49 -0300

    melhoria no loggin

--- Controladores/longincontroller.js
+++ Controladores/longincontroller.js
@@ -28,27 +28,92 @@ const user = {
         }}
     },
     login: async (req, res, next)=>{
-        const userEmail = await User.findOne({email: req.body.email})
-        if (!userEmail) return res.status(400).send('Email ou senha incorreto')
-        const passwordAndUserMatch = bcrypt.compare(req.body.password, userEmail.password)
-        .then(async (e)=>{
-            if (!e) return res.status(400).send({msg:'Email ou senha incorreto'})
-            const accessToken = jwt.sign(
-                {email: userEmail.email, admin: userEmail.admin},
-                process.env.TOKEN_SECRET,
-                { expiresIn: '30s' }
-            );
-            const refreshToken = jwt.sign(
-                {email: userEmail.email, admin: userEmail.admin},
-                process.env.REFRESH_TOKEN_SECRET,
-                { expiresIn: '1d' }
-            );
-            User.findOneAndUpdate({email: req.body.email}, {refreshToken:refreshToken}, (err, doc)=>{console.log(err)})
-            res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
-            res.json({ accessToken })
-            console.log(accessToken);
+
+    const cookies = req.cookies;
+
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    const foundUser = await User.findOne({ email: email }).exec();
+    if (!foundUser) return res.sendStatus(401); //Unauthorized 
+    // evaluate password 
+    const match = await bcrypt.compare(password, foundUser.password);
+    if (match) {
+        // const roles = Object.values(foundUser.roles).filter(Boolean);
+        // create JWTs
+        const accessToken = jwt.sign(
+            {
+                email: foundUser.email, admin: foundUser.admin
+            },
+            process.env.TOKEN_SECRET,
+            { expiresIn: '10s' }
+        );
+        const newRefreshToken = jwt.sign(
+            {email: foundUser.email, admin: foundUser.admin},
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        // Changed to let keyword
+        let newRefreshTokenArray =
+            !cookies?.jwt
+                ? foundUser.refreshToken
+                : foundUser.refreshToken.filter(rt => rt !== cookies.jwt);
+
+        if (cookies?.jwt) {
+
+            /* 
+            Scenario added here: 
+                1) User logs in but never uses RT and does not logout 
+                2) RT is stolen
+                3) If 1 & 2, reuse detection is needed to clear all RTs when user logs in
+            */
+            const refreshToken = cookies.jwt;
+            const foundToken = await User.findOne({ refreshToken }).exec();
+
+            // Detected refresh token reuse!
+            if (!foundToken) {
+                // clear out ALL previous refresh tokens
+                newRefreshTokenArray = [];
+            }
+
+            res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
         }
-    )
+
+        // Saving refreshToken with current user
+        foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+        const result = await foundUser.save();
+
+        // Creates Secure Cookie with refresh token
+        res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+
+        // Send authorization roles and access token to user
+        res.json({ accessToken });
+
+    } else {
+        res.sendStatus(401);
+    }
+    },
+    loggout: async (req, res, next)=>{
+
+        const cookies = req.cookies;
+        if (!cookies?.jwt) return res.sendStatus(204); //No content
+        const refreshToken = cookies.jwt;
+        // Is refreshToken in db?
+        const foundUser = await User.findOne({ refreshToken }).exec();
+        if (!foundUser) {
+            res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+            return res.sendStatus(204);
+        }
+    
+        // Delete refreshToken in db
+        foundUser.refreshToken = foundUser.refreshToken.filter(rt => rt !== refreshToken);;
+        const result = await foundUser.save();
+        console.log(result);
+    
+        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+        res.sendStatus(204);
+
     }
 }
 
--- rotas/refresh.js
+++ rotas/refresh.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const refreshTokenController = require('../Controladores/refreshTokenController');
+const longincontroller = require ('../Controladores/longincontroller')
 
 router.get('/', refreshTokenController.handleRefreshToken);
+router.get('/logout', longincontroller.loggout);
 
 module.exports = router;
