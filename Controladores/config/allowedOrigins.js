4abe44d02aa2768521d72afe3d76e0cacce364bf
Aurindo Neto <105182999+aurindo10@users.noreply.github.com>
2022-10-30 16:34:57 -0300

    finalfinal

--- Controladores/config/allowedOrigins.js
+++ Controladores/config/allowedOrigins.js
@@ -2,7 +2,9 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5500',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'https://cotacaots-1qng4mcbt-aurindo10.vercel.app',
+    'https://cotacaots.vercel.app/'
 ];
 
 module.exports = allowedOrigins;
