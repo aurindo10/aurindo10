019b6adfcb439e4ea3623be315de76838e5fa76c
Aurindo Neto <105182999+aurindo10@users.noreply.github.com>
2022-11-07 22:01:29 -0300

    dsadasda

--- Models/vendedor.js
+++ Models/vendedor.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose') ;
 const vendedor  = mongoose.Schema({
-    nome: {type: String, required: true, minlength: 3, maxlenght: 50},
-    empresa: {type: String, required: true, minlength: 3, maxlenght: 100},
+    nome: {type: String, required: true, minlength: 1, maxlenght: 50},
+    empresa: {type: String, required: true, maxlenght: 100},
     createdAt: {type: Date, default: Date.now},
 })
 
